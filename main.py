60690b62738ff2438bd305ca63427623bb609514
agzamkhuja <89140072+Ameliyev@users.noreply.github.com>
2022-01-02 09:02:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+from replit import db
+from replit import db
+from replit import db
+print("name", "age")
+"main.py"
+print("Agzamkhuja", "22")
+"main.py"
+print("brithday", "education")
+"main.py"
+print("29.07.1999", "high school diploma")
+"main.py"
+
+id(2)
+
+print("your location", "company")
+"main.py"
+print("Tashkent Uzbekistan", "New Uzbekistan")
+"main.py"
+
+print("school 90", "class 9")
+"main.py"
+
+str
+
+print("date", "mounth", "year")
+"main.py"
+print("29", "12", "2021")
+"main.py"
+
+print("uername", "password")
+"main.main.py="
+GeneratorExit
+
+import cProfile
+
+print("agzam")
+"main.main.py="
+print("Azamxoja19")
+"main.main.py"
+
+import funcsigs
+{1, 2, 3}
+
+set
+
+global imp
+
+cProfile
+RuntimeWarning
+
+print("new project+new idea")
+"main.py="
+print("folder", "trash", "list")
+"main.main.py="
+
+#ModuleNotFoundError
+
+ProcessLookupError
+
+tuple
+
+{1, 2, 3, 3, 4, 4, 5, ConnectionError}
+
+print("I love you Allah", "I love Muhammad")
+"main.py"
+print("Uzbekistan has situated Central Asia" , "Tashkent is capital of the Republic of Uzbekistan")
+"main.py"
+
+
+
---
+++ main.py=/main.py
@@ -0,0 +1,8 @@
+print("course" , "project")
+"main.py"
+object 
+
+ProcessLookupError
+
+print("banana" , "banana tree")
+"main.py"
